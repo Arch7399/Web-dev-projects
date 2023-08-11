37dfd016d155310654e2059e962fcd6fdf7d003d
Arch7399 <raymond17399@gmail.com>
2023-08-11 10:09:47 +0530

    user click detection, audio, fade animation

--- Simon-Game/game.js
+++ Simon-Game/game.js
@@ -1,8 +1,25 @@
 var buttonColors = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
+var userClickedPattern = [];
+
+function playSound(name) {
+    var sound = new Audio("sounds/"+name+".mp3");
+    sound.play();
+}
+
 function nextSequence() {
     var randomNumber = Math.floor(Math.random() * 4);
     var randomChosenColor = buttonColors[randomNumber];
     gamePattern.push(randomChosenColor);
-    return gamePattern;
+    $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+    var userChosenColour = $("#" + randomChosenColor).attr("id");
+    console.log(userChosenColour);
+    playSound(randomChosenColor);    
 }
+
+$(".btn").on("click", function(e){
+    var userChosenColour = e.target.id;
+    userClickedPattern.push(userChosenColour);
+    playSound(userChosenColour);
+});
+
