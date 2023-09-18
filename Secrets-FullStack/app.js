3bb25afcffceaa315190719decd5dff830a367a4
Arch7399 <raymond17399@gmail.com>
2023-09-18 15:10:27 +0530

    Added password encryption using mongoose-encryption npm

--- Secrets-FullStack/app.js
+++ Secrets-FullStack/app.js
@@ -1,9 +1,9 @@
-//jshint esversion:6
 import express from "express";
 import ejs from "ejs";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import _ from "lodash";
+import encrypt from "mongoose-encryption";
 
 const app = express();
 const port = 3000;
@@ -14,10 +14,14 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 mongoose.connect("mongodb://0.0.0.0:27017/userAuthDB");
 
-const userSchema = {
+const userSchema = new mongoose.Schema({
     username: String,
     password: String
-};
+});
+
+const secret = "maadupattunim";
+
+userSchema.plugin(encrypt, {secret: secret, encryptedFields: ["password"]});
 
 const Credentials = mongoose.model("credentials", userSchema);
 
@@ -33,6 +37,10 @@ app.get("/register", (req, res) => {
     res.render("register",{});
 });
 
+app.get("/secrets", (req, res) => {
+    res.render("secrets",{});
+});
+
 app.post("/register", (req, res) => {
     const userName = _.lowerCase(req.body.username);
     const passWord = req.body.password;
@@ -40,6 +48,7 @@ app.post("/register", (req, res) => {
     Credentials.findOne({username: userName}).then(function(foundName){
         if(foundName){
             console.log("This username is already taken...");
+            res.redirect("/register");
         } else {
             const newUser = new Credentials({
                 username: userName,
@@ -47,6 +56,27 @@ app.post("/register", (req, res) => {
             });
             newUser.save();
             console.log("User registered successfully..!");
+            res.redirect("/secrets");
+        }
+    });
+});
+
+app.post("/login", (req, res) => {
+    const userName = _.lowerCase(req.body.username);
+    const passWord = req.body.password;
+
+    Credentials.findOne({username: userName}).then(function(foundName){
+        if(foundName){
+            if(foundName.password === passWord){
+                console.log("User logged in successfully..!");
+            res.redirect("/secrets");
+            } else {
+                console.log("Wrong Password");
+                res.redirect("/login");
+            }
+        } else {
+            console.log("Wrong credentials");
+            res.redirect("/login");
         }
     });
 });
